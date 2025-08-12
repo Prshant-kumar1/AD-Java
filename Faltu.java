7c58bda1ce011e13e0fe1702ffa54e20ca833bb8
Prshant-kumar1 <work.prashant12@gmail.com>
2025-08-12 10:52:10 +0530

    Add files via upload

---
+++ Faltu.java
@@ -0,0 +1,19 @@
+public class Faltu {
+    public static void main(String[] args) {  // Main method to execute the program
+        int a = 65;  // Initialize variable a
+        int b = 48;  // Initialize variable b
+        int sum = a + b;  // Calculate the sum of a and b
+        System.out.println("the sum is: "+sum);  // Print the sum
+
+
+        
+        System.out.println("Byte: " + Byte.MIN_VALUE + " to " + Byte.MAX_VALUE);
+        System.out.println("Short: " + Short.MIN_VALUE + " to " + Short.MAX_VALUE);
+        System.out.println("Integer: " + Integer.MIN_VALUE + " to " + Integer.MAX_VALUE);
+        System.out.println("Long: " + Long.MIN_VALUE + " to " + Long.MAX_VALUE);
+        System.out.println("Float: " + Float.MIN_VALUE + " to " + Float.MAX_VALUE);
+        System.out.println("Double: " + Double.MIN_VALUE + " to " + Double.MAX_VALUE);
+        System.out.println("Char: "+(int)Character.MIN_VALUE +" to "+(int)Character.MAX_VALUE);
+
+    }
+}
